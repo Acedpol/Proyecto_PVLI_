78fa77365a593f03c8558d8611edbdeb53821c21
Oscar <oscarped@ucm.es>
2020-12-03 22:39:27 +0100

    Borrado Player2

--- proyecto_pvli_/src/Game.js
+++ proyecto_pvli_/src/Game.js
@@ -1,5 +1,5 @@
 import Player from './Player.js';
-import Player2 from './Player2.js';
+
 
 export default class Game extends Phaser.Scene {
   constructor() {
@@ -11,9 +11,8 @@ export default class Game extends Phaser.Scene {
     this.add.image(this.scale.width / 2, this.scale.height / 2, 'burro');
     this.add.text((this.scale.width / 3 + 125), this.scale.height / 3, "¡Hola, Soy Platero!", { fontColor: 0x76C9F0 });
     this.player = new Player(this, 300, 200, 'player');
-    this.player2=new Player2(this,600,200,'player');
     this.cameras.main.startFollow(this.player);
-    this.physics.add.collider(this.player,this.player2);
+    this.physics.add.collider(this.player);
   }
 
   update(time, delta) {
