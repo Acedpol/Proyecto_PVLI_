15b97595f896f820a177b7c0abb0737ec2230ccd
Oscar <oscarped@ucm.es>
2020-12-10 20:20:28 +0100

    barra de vida modificada

--- proyecto_pvli_/src/health.js
+++ proyecto_pvli_/src/health.js
@@ -4,16 +4,18 @@ export default class health extends Phaser.GameObjects.Sprite {
       super(scene, x, y, texture, 0);
       //this.setOrigin(200,300);
       this.displayOriginX=0;
-      this.displayWidth=this.scene.player.health;
-      this.wi=this.scene.player.health;
-      this.displayHeight=5;
-      this.x=this.x-(this.wi/2);
+      this.displayWidth=300;
+      //this.wi=this.scene.player.health;
+      this.displayHeight=50;
+      this.x=this.scene.player.x-700;
+      this.y=this.scene.player.y-380;
       this.scene.add.existing(this);      
     }
     preUpdate(){
-        this.x=this.scene.player.x-(this.wi/2);
-        this.y=this.scene.player.y-30;
-
-        this.displayWidth=this.scene.player.health;
+      this.x=this.scene.player.x-700;
+      this.y=this.scene.player.y-400;
+       // this.x=this.scene.player.x-(this.wi/2);
+       // this.y=this.scene.player.y-30;
+        this.displayWidth=this.scene.player.health*300/100;
     }
   }
