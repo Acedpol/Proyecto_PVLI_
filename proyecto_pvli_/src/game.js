41b58c4de5bd386a246f3d3aaa9d1094af200843
Oscar <oscarped@ucm.es>
2020-12-10 22:54:31 +0100

    barra de vida acabada

--- proyecto_pvli_/src/game.js
+++ proyecto_pvli_/src/game.js
@@ -13,7 +13,7 @@ export default class game extends Phaser.Scene {
     this.add.image(this.scale.width / 2, this.scale.height / 2, 'burro');
     this.add.text((this.scale.width / 3 + 125), this.scale.height / 3, "¡Hola, Soy Platero!", { fontColor: 0x76C9F0 });
     this.player = new player(this, 300, 200, "player");
-    this.live = new health(this,this.player.x,this.player.y-30,"live");
+    this.live = new health(this,0,25,"live");
     this.cameras.main.startFollow(this.player);
     this.physics.add.collider(this.player);
     this.pausemenu  = new pausemenu(this, this.player.x, this.player.y, "libro")
--- proyecto_pvli_/src/health.js
+++ proyecto_pvli_/src/health.js
@@ -2,20 +2,13 @@
 export default class health extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture) {
       super(scene, x, y, texture, 0);
-      //this.setOrigin(200,300);
       this.displayOriginX=0;
       this.displayWidth=300;
-      //this.wi=this.scene.player.health;
+      this.setScrollFactor(0);
       this.displayHeight=50;
-      this.x=this.scene.player.x-700;
-      this.y=this.scene.player.y-380;
       this.scene.add.existing(this);      
     }
     preUpdate(){
-      this.x=this.scene.player.x-700;
-      this.y=this.scene.player.y-400;
-       // this.x=this.scene.player.x-(this.wi/2);
-       // this.y=this.scene.player.y-30;
         this.displayWidth=this.scene.player.health*300/100;
     }
   }
