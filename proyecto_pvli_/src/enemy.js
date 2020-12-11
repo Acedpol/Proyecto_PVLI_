34f7668d16cf721cbdffc66c5d8ff25d090b7452
Oscar <oscarped@ucm.es>
2020-12-11 18:24:26 +0100

    Enemigo que quita vida

--- proyecto_pvli_/src/enemy.js
+++ proyecto_pvli_/src/enemy.js
@@ -3,9 +3,17 @@ export default class enemy extends Phaser.GameObjects.Sprite {
       super(scene, x, y, texture, 0);
       this.scene.add.existing(this);
       this.scene.physics.add.existing(this);
+      this.body.setImmovable();
       this.body.setCollideWorldBounds();
       this.speed = 300;
       this.area = 200;
     }
-    
-  }
+    preUpdate(){
+      this.quitaVida();
+    }
+    quitaVida(){
+      if(this.scene.player.y<this.y+this.area && this.scene.player.y>this.y && this.scene.player.x>this.x-5&&this.scene.player.x<this.x+5){
+        this.scene.player.health--;
+      }
+    }
+ }
--- proyecto_pvli_/src/game.js
+++ proyecto_pvli_/src/game.js
@@ -1,6 +1,7 @@
 import player from './player.js';
 import health from './health.js';
 import pausemenu from './pausemenu.js';
+import enemy from './enemy.js';
 
 export default class game extends Phaser.Scene {
   constructor() {
@@ -13,17 +14,19 @@ export default class game extends Phaser.Scene {
     this.add.image(this.scale.width / 2, this.scale.height / 2, 'burro');
     this.add.text((this.scale.width / 3 + 125), this.scale.height / 3, "¡Hola, Soy Platero!", { fontColor: 0x76C9F0 });
     this.player = new player(this, 300, 200, "player");
-    this.live = new health(this,0,25,"live");
+    
+    this.enemy=new enemy(this,300,400,"player");
     this.cameras.main.startFollow(this.player);
-    this.physics.add.collider(this.player);
+    this.physics.add.collider(this.player,this.enemy);
     this.pausemenu  = new pausemenu(this, this.player.x, this.player.y, "libro")
     this.pausemenu.scale = 0.8;
-    
+    this.live = new health(this,0,25,"live");
     this.input.keyboard.addKey('ESC').on('down', event => { this.pause() });
   }
 
   update(time, delta) {
     
+    console.log(this.player.health);
   }
 
   pause(){
