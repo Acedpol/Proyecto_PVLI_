c59b9c405f20ed00ac289fe4d1b99d90739d563a
Cristian <crisca06@ucm.es>
2021-01-19 17:10:41 +0100

    Mejorados los triangulos, aunque no del todo bien hechos, les queda poco

--- proyecto_pvli_/src/zone.js
+++ proyecto_pvli_/src/zone.js
@@ -2,37 +2,23 @@
 export default class zone extends Phaser.Physics.Matter.Sprite {
     constructor(scene, x, y, texture, h, dir, enemy) {
         super(scene.matter.world,x, y, texture, h,dir, 0);
-       
+        
+        this.polygonsize = 100;
+
         //this.setOrigin(0,0);
-        this.setScale(0.5,0.3);
-        this.setPolygon(100,3);
+        this.setScale(0.25,0.25);
+        this.setPolygon(this.polygonsize,3);
         //this.body.setScale(this.width,this.height);
         // console.log(this.body);
         this.setStatic(true);
         this.dir=dir;
         this.enemType = enemy;
-        if(this.dir == 0) {
-            //this.body.angle=30;
-            this.angle=0;
-        }
-        else if(this.dir == 1) {
-        // this.body.angle=225;
-           this.angle=180;
-        }
-        else if(this.dir == 2) {
-            //this.body.angle=135;
-            this.angle=90;
-        }
-        else if(this.dir == 3) {
-            //this.body.angle=315;
-            this.angle=270;
-        }
+        
         // this.scene.physics.add.existing(this);
         //this.scene.matter.add.existing(this);
         this.setDepth(-1);
-        this.body.angle=30-0.27;
-      
-        // this.body.angle=this.angle;
+        
+        this.body.angle= -1.5708; //90 grados en radianes
         /* this.shape = this.scene.cache.json.get('zonePhy');*/
         this.scene.add.existing(this);
     }
@@ -41,12 +27,31 @@ export default class zone extends Phaser.Physics.Matter.Sprite {
        // this.scene.matter.add.image(100,100,'zone');
         this.changeAngle();
         this.move();
-        this.updateBody();
     }
 
     move() {
-        this.x=this.scene.enemies.getChildren()[this.enemType].x;
-        this.y=this.scene.enemies.getChildren()[this.enemType].y + (this.scene.enemies.getChildren()[this.enemType].height/2); 
+        this.mainenemy = this.scene.enemies.getChildren()[this.enemType];
+        switch(this.mainenemy.typeMov){
+            case 0:
+                if(this.mainenemy.dir === 3){
+                this.x=this.mainenemy.x - this.polygonsize/2 - this.mainenemy.width;
+                this.y=this.mainenemy.y;
+                }
+                else if(this.mainenemy.dir === 2){
+                this.x=this.mainenemy.x + this.polygonsize/2 + this.mainenemy.width;
+                this.y=this.mainenemy.y;
+                }
+            break; 
+            case 1:
+                if(this.mainenemy.dir === 0){
+                    this.x=this.mainenemy.x;
+                    this.y=this.mainenemy.y + this.polygonsize/2 + this.mainenemy.height;
+                }
+                else if(this.mainenemy.dir === 1){
+                    this.x=this.mainenemy.x;
+                    this.y=this.mainenemy.y - this.polygonsize/2 - this.mainenemy.height;
+                }
+        }
     }
 
     changeAngle() {
@@ -60,19 +65,11 @@ export default class zone extends Phaser.Physics.Matter.Sprite {
         }
         else if(this.scene.enemies.getChildren()[this.enemType].dir == 2) {
            // this.body.angle=135;
-            this.angle = 90;
+            this.angle = 270;
         }
         else if(this.scene.enemies.getChildren()[this.enemType].dir == 3) {
             //this.body.angle=315;
-            this.angle = 270;
+            this.angle = 90;
         }
-    }
-
-    updateBody() {
-        this.body.width = this.width;
-        this.body.height = this.height;
-        this.body.x = this.x;
-        this.body.y = this.y;
-        //this.body.angle=this.angle;
     }
 }
