af91a61feeacaa3f66f0706fea594e790c5b5151
Cristian <crisca06@ucm.es>
2021-01-20 11:54:13 +0100

    Ahora se cogen solo si no tienes bateria

--- proyecto_pvli_/src/batery.js
+++ proyecto_pvli_/src/batery.js
@@ -3,9 +3,11 @@ import item from './item.js';
 export default class batery extends item {
 
     catchObject(){
-            this.scene.sound.play('pickupsound')
-           // this.scene.contadorPilas++;
-            this.scene.radio.duration=this.scene.radio.maxDuration;
-            this.destroy();
+
+        if(this.scene.radio.duration<this.scene.radio.maxDuration){
+        this.scene.sound.play('pickupsound');
+        this.scene.radio.duration=this.scene.radio.maxDuration;
+        this.destroy();
+        }
     }
 }
