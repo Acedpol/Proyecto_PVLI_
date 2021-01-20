711ec64ce636051855e18d68f2e01e09ec5b783a
Cristian <crisca06@ucm.es>
2021-01-20 02:10:46 +0100

    Borrado algo de codigo que no utilizamos y limpiado un poco el zone.js

--- proyecto_pvli_/src/zone.js
+++ proyecto_pvli_/src/zone.js
@@ -5,27 +5,22 @@ export default class zone extends Phaser.Physics.Matter.Sprite {
         
         this.polygonsize = 100;
 
-        //this.setOrigin(0,0);
         this.setScale(1.7, 1.7);
         this.setAlpha(0.6);
         this.setPolygon(this.polygonsize,3);
-        //this.body.setScale(this.width,this.height);
-        // console.log(this.body);
+
         this.setStatic(true);
         this.dir=dir;
         this.enemType = enemy;
         
-        // this.scene.physics.add.existing(this);
-        //this.scene.matter.add.existing(this);
         this.setDepth(-1);
         
         this.body.angle= -1.5708; //90 grados en radianes
-        /* this.shape = this.scene.cache.json.get('zonePhy');*/
         this.scene.add.existing(this);
     }
 
     preUpdate() {
-       // this.scene.matter.add.image(100,100,'zone');
+        //Si el enemigo se encuentra parado, este también se parará
         if(!this.scene.enemies.getChildren()[this.enemType].pause){
             this.changeAngle();
             this.move();
@@ -33,6 +28,8 @@ export default class zone extends Phaser.Physics.Matter.Sprite {
     }
 
     move() {
+        //En funcion de la dirección por la que mire el enemigo la zona se moverá con él
+
         this.mainenemy = this.scene.enemies.getChildren()[this.enemType];
         switch(this.mainenemy.typeMov){
             case 0:
@@ -58,21 +55,19 @@ export default class zone extends Phaser.Physics.Matter.Sprite {
     }
 
     changeAngle() {
+        //En funcion de la direccion del enemigo también se moverá el angulo de la zona
+        
         this.enemydir = this.scene.enemies.getChildren()[this.enemType].dir;
         if(this.enemydir == 0) {
-           // this.body.angle=45;
             this.angle = 0;
         }
         else if(this.enemydir == 1) {
-            //this.body.angle=225;
             this.angle = 180;
         }
         else if(this.enemydir == 2) {
-           // this.body.angle=135;
             this.angle = 270;
         }
         else if(this.enemydir == 3) {
-            //this.body.angle=315;
             this.angle = 90;
         }
     }
